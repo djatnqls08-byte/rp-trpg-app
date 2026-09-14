f8f781db2eee491d80e6346fd7bb093b4bf8d02d
djatnqls08-byte <djatnqls08@gmail.com>
2026-09-15 05:22:52 +0900

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5906,9 +5906,9 @@ const isSanCheckDetected = activeSession?.ruleMode === "coc" && !activeSession?.
                       </>
                     )}
                   </div>
+                </div>
 
-
-                  {/* 2. 중앙: 테두리 없는 투명 textarea */}
+                {/* 2. 중앙: 테두리 없는 투명 textarea */}
                 <textarea 
                   value={input} 
                   onChange={e => {
